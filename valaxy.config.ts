1ba0585508676760ccea1cd1871a8d1752ad5d8c
zephyruscc <124758878+zephyruscc@users.noreply.github.com>
2024-10-22 21:30:11 +0800

    :pencil: update content

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -17,31 +17,31 @@ export default defineValaxyConfig<UserThemeConfig>({
   themeConfig: {
     banner: {
       enable: true,
-      title: '云游君的小站',
+      title: '我永远喜欢千束',
     },
 
-    pages: [
-      {
-        name: '我的小伙伴们',
-        url: '/links/',
-        icon: 'i-ri-genderless-line',
-        color: 'dodgerblue',
-      },
-      {
-        name: '喜欢的女孩子',
-        url: '/girls/',
-        icon: 'i-ri-women-line',
-        color: 'hotpink',
-      },
-    ],
+    // pages: [
+    //   {
+    //     name: '我的小伙伴们',
+    //     url: '/links/',
+    //     icon: 'i-ri-genderless-line',
+    //     color: 'dodgerblue',
+    //   },
+    //   {
+    //     name: '喜欢的女孩子',
+    //     url: '/girls/',
+    //     icon: 'i-ri-women-line',
+    //     color: 'hotpink',
+    //   },
+    // ],
 
-    footer: {
-      since: 2016,
-      beian: {
-        enable: true,
-        icp: '苏ICP备17038157号',
-      },
-    },
+    // footer: {
+    //   since: 2016,
+    //   beian: {
+    //     enable: true,
+    //     icp: '苏ICP备17038157号',
+    //   },
+    // },
   },
 
   unocss: { safelist },
